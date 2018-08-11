a1bff94b29dab3b1622f33053177a9d45b70f956
Lekshmi07 <rajalekshmi77277@gmail.com>
2018-08-11 22:00:38 +0530

    latest

--- app/src/main/java/kwa/pumps/switchthepump/ViewShift.java
+++ app/src/main/java/kwa/pumps/switchthepump/ViewShift.java
@@ -22,36 +22,46 @@ public class ViewShift extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_shift);
 
-        tableLayout=(TableLayout)findViewById(R.id.viewtable);
-        db=new DBManager(ViewShift.this);
+        tableLayout = (TableLayout) findViewById(R.id.viewtable);
+        db = new DBManager(ViewShift.this);
 
-        int numrows=db.numOfRows();
-        Cursor cursor=db.viewData();
-        if(cursor.getCount()!=0) {
-            for (int i = 0; i < numrows; i++) {
-                View tableRow = LayoutInflater.from(this).inflate(R.layout.activity_view_shift, null, false);
-                TextView Num = (TextView) tableRow.findViewById(R.id.Number);
-                TextView Power = (TextView) tableRow.findViewById(R.id.power);
-                TextView Pump = (TextView) tableRow.findViewById(R.id.pump);
-                TextView ON = (TextView) tableRow.findViewById(R.id.ON);
-                TextView OFF = (TextView) tableRow.findViewById(R.id.OFF);
+        int numrows = db.numOfRows();
+        Cursor cursor = db.viewData();
 
-                String number_str=cursor.getString(cursor.getColumnIndex(db.MOBILE_NO));
-                String power_str=cursor.getString(cursor.getColumnIndex(db.POWER));
-                String pump_str=cursor.getString(cursor.getColumnIndex(db.PUMP));
-                String on_str=cursor.getString(cursor.getColumnIndex(db.TIME_ON));
-                String off_str=cursor.getString(cursor.getColumnIndex(db.TIME_OFF));
+        if (numrows != 0) {
+                try {
+                    if (cursor.getCount() > 0) {
+                        while (cursor.moveToNext()) {
 
 
-                Num.setText(number_str);
-                Power.setText(power_str);
-                Pump.setText(pump_str);
-                ON.setText(on_str);
-                OFF.setText(off_str);
-                tableLayout.addView(tableRow);
-            }
+                            String number_str = cursor.getString(cursor.getColumnIndex(db.MOBILE_NO));
+                            String power_str = cursor.getString(cursor.getColumnIndex(db.POWER));
+                            String pump_str = cursor.getString(cursor.getColumnIndex(db.PUMP));
+                            String on_str = cursor.getString(cursor.getColumnIndex(db.TIME_ON));
+                            String off_str = cursor.getString(cursor.getColumnIndex(db.TIME_OFF));
+
+                            View tableRow = LayoutInflater.from(this).inflate(R.layout.activity_view_shift, null, false);
+                            TextView Num = (TextView) tableRow.findViewById(R.id.Number);
+                            TextView Power = (TextView) tableRow.findViewById(R.id.power);
+                            TextView Pump = (TextView) tableRow.findViewById(R.id.pump);
+                            TextView ON = (TextView) tableRow.findViewById(R.id.ON);
+                            TextView OFF = (TextView) tableRow.findViewById(R.id.OFF);
+
+                            Num.setText(number_str.toString());
+                            Power.setText(power_str.toString());
+                            Pump.setText(pump_str.toString());
+                            ON.setText(on_str.toString());
+                            OFF.setText(off_str.toString());
+                            tableLayout.addView(tableRow);
+
+                        }
+                    }
+                }catch (Exception e){
+
+                }
+
+
+
         }
-
-
     }
 }
